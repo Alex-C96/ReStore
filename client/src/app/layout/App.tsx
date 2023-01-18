ee62e2a2256420b1b7c93c1999a5dd2f08c3e8ff
Alex-C96 <alexander15clarke@gmail.com>
2023-01-17 21:54:53 -0600

    Add header

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,7 +1,14 @@
-import { Typography } from "@mui/material";
+import { Container, createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 import { useEffect, useState } from "react";
 import Catalog from "../../features/catalog/Catalog";
 import { Product } from "../models/products";
+import Header from "./Header";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
 function App() {
   const [products, setProducts] = useState<Product[]>([]);
@@ -25,10 +32,13 @@ function App() {
   }, [])
 
   return (
-    <>
-      <Typography variant='h1'>Re-Store</Typography>
-      <Catalog products={products} addProduct={addProduct}/>    
-    </>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Header />
+      <Container>
+        <Catalog products={products} addProduct={addProduct}/> 
+      </Container>
+    </ThemeProvider>
   );
 }
 
