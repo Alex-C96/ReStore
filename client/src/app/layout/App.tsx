c7bb55b55f6299fd3c6267b6a434b17ae00b6dd3
Alex-C96 <alexander15clarke@gmail.com>
2023-01-23 22:55:17 -0600

    Add new paths and set up React Router.

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,7 +1,18 @@
 import { Container, createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 import { useState } from "react";
+import { Route } from "react-router-dom";
+import AboutPage from "../../features/about/AboutPage";
 import Catalog from "../../features/catalog/Catalog";
+import ProductDetails from "../../features/catalog/ProductDetails";
+import ContactPage from "../../features/contact/ContactPage";
+import HomePage from "../../features/home/HomePage";
 import Header from "./Header";
+
+declare module '@mui/material' {
+    interface ThemeOptions {    
+        palette?: object
+    }
+}
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
@@ -24,7 +35,12 @@ function App() {
       <CssBaseline />
       <Header darkMode={darkMode} switchHandler={switchHandler}/>
       <Container>
-        <Catalog /> 
+        <Route exact path='/' component={HomePage} /> 
+        <Route exact path='/catalog' component={Catalog} /> 
+        <Route path='/catalog/:id' component={ProductDetails} /> 
+        <Route path='/about' component={AboutPage} /> 
+        <Route path='/contact' component={ContactPage} /> 
+
       </Container>
     </ThemeProvider>
   );
